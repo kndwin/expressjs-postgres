205a710584333ce1ef6c243b53285e503a9f4af5
Kevin Nguyen <me@kndwin.dev>
2021-06-26 17:13:30 +1000

    ➕ - feat: added redis, postgres

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,39 @@
+const { promisify } = require('util')
 import bodyParser from "body-parser";
+import dotenv from 'dotenv'
+import redis from 'redis'
 import express from "express";
 import pg from "pg";
 
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
+dotenv.config()
+// Connect to the database using the DATABASE_URL environment variable injected by Railway
 const pool = new pg.Pool();
 
 const app = express();
+const client = redis.createClient({ url: process.env.REDIS_URL})
 const port = process.env.PORT || 3333;
+
+const getAsync = promisify(client.get).bind(client)
+const setAsync = promisify(client.set).bind(client)
 
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
+app.get("/", async (_, res) => {
   const { rows } = await pool.query("SELECT NOW()");
   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
 });
+
+app.get('/redis', async (_, res) => {
+	try {
+		let visits = await getAsync('visits') | 0
+		res.send(`Visit: ${visits}`)
+		setAsync('visits', parseInt(visits.toString()) + 1)
+	} catch (err) {
+		console.error(err)
+	}
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
